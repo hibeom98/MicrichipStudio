fdd05f2ee74d53adbe4befc66d4a2b15f5a293a4
hibeom98 <108720396+hibeom98@users.noreply.github.com>
2023-05-16 09:53:22 +0900

    Add files via upload

---
+++ 230511/USART/USART/ULED.h
@@ -0,0 +1,13 @@
+﻿
+
+#ifndef ULED_H_
+#define ULED_H_
+
+void UART0_init(void);
+void UART0_transmit(char data);	     //UART가 2개인데 그 중에서 우리는 0번을 쓸 꺼라서 0을 붙인다.
+unsigned char UART0_receive(void);   //초기화 하는 함수                    //데이터 시트 191p
+
+
+
+
+#endif /* ULED_H_ */
